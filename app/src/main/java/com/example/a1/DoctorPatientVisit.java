9f65587a0129562f5ce9b7a825e8cbaa731439dc
shaminda1017 <abeyseks@uwindsor.ca>
2024-03-01 23:47:51 -0500

    Added pages for list view

---
+++ app/src/main/java/com/example/a1/DoctorPatientVisit.java
@@ -0,0 +1,27 @@
+package com.example.a1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoctorPatientVisit extends AppCompatActivity {
+
+    Button backBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patient_visit);
+
+        backBtn = findViewById(R.id.back_to_home_button);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
